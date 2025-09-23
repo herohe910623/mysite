a4a7f064b0053bbc23e7dc022d45998a71cc39ec
gijin <herohe910623@gmail.com>
2025-09-23 16:23:52 +0900

    질문등록 localhost/pybo 페이지 요청시 등록한 Question_list 들을 조회할 수 있도록 변경

--- pybo/views.py
+++ pybo/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신걸 환영합니다.")
-    
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+# Create your views here.
+# from django.http import HttpResponse
+#
+# def index(request):
+#     return HttpResponse("안녕하세요 pybo에 오신걸 환영합니다.")
+#
