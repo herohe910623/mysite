b858feee195db599a3f6460b6765ed8efcfa3cb9
gijin <herohe910623@gmail.com>
2025-09-23 17:13:32 +0900

    view.py 파일에 views.detail 함수를 만들어주었다.

--- pybo/views.py
+++ pybo/views.py
@@ -6,6 +6,11 @@ def index(request):
     context = {'question_list': question_list}
     return render(request, 'pybo/question_list.html', context)
 
+def detail(request, question_id):
+    question = Question.objects.get(id=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
 # Create your views here.
 # from django.http import HttpResponse
 #
