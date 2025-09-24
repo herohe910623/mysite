b38d7415b8f5ed6f127e4da13086e7b4924a888e
gijin <herohe910623@gmail.com>
2025-09-24 11:09:09 +0900

    HTMLTemplates 를 만들어서 재활용 def를 만들었다.

--- test/urls.py
+++ test/urls.py
@@ -4,5 +4,5 @@ from test import views
 urlpatterns = [
     path('', views.index),
     path('create/',views.create),
-    path('read/1/',views.read)
+    path('read/<id>/',views.read)
 ]
--- test/views.py
+++ test/views.py
@@ -1,11 +1,40 @@
 from django.shortcuts import render, HttpResponse
+import random
+
+topics = [
+    {'id': 1, 'title': 'routing', 'body': 'Routing is...'},
+    {'id': 2, 'title': 'view', 'body': 'view is...'},
+    {'id': 3, 'title': 'moodel', 'body': 'moddel is...'}
+]
+
 
 # Create your views here.
+def HTMLTemplate():
+    global topics
+    ol = ''  ##변수 생성
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    return f'''
+    <html>
+    <body>
+    <h1>Django</h1>
+    <ol>
+        {ol}
+    </ol>
+    <h2>Welcome</h2>
+    Hello Django!
+    </body>
+    </html>
+    '''
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse(HTMLTemplate())
+
 
 def create(request):
     return HttpResponse("Create!")
 
-def read(request):
-    return HttpResponse("Read!")
+
+def read(request, id):
+    return HttpResponse("Read!" + id)
