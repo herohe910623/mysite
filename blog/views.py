61aadd5e8b0181a3065bbcf04e6f96aa6eb7974c
herohe910623 <herohe910623@gmail.com>
2025-10-09 18:48:28 +0900

    CBV로 블로그 포스트 목록 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    # template_name = 'blog/index.html' CBV 사용하여 주석처리 ? 인식이 된다? 이름을 찾아간다 post_list.html
 
 # Create your views here.
 # def index(request):
 #     return render(request, 'blog/index.html',)
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts':posts,
-        }
-    )
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts':posts,
+#         }
+#     )
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
